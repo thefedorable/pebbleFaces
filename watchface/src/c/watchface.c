fbf8602840166934b7859715094bbe19b62d9e66
ttan421 <tanner.hartwell421@gmail.com>
2025-03-27 17:45:04 -0600

    Add gitignore

--- watchface/src/c/watchface.c
+++ watchface/src/c/watchface.c
@@ -1,36 +1,118 @@
 #include <pebble.h>
 
+// Forward declarations
+static void main_window_load(Window *window);
+static void main_window_unload(Window *window);
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed);
+static void update_time(void);
+
 static Window *s_main_window;
+static TextLayer *s_time_layer;
+static TextLayer *s_date_layer;
+
+// Declare globally
+static GFont s_time_font;
+static GFont s_date_font;
 
 static void main_window_load(Window *window) {
+  // Get information about the Window
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
 
+  // Create the TextLayer with specific bounds
+  s_time_layer = text_layer_create(
+      GRect(5, 110, bounds.size.w, 50));
+
+  // Improve the layout to be more like a watchface
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_38));
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorBlack);
+  text_layer_set_font(s_time_layer, s_time_font);
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+
+  // Create the TextLayer for date
+  s_date_layer = text_layer_create(
+    GRect(0, 150, bounds.size.w, 25));
+
+  // Style the text
+  s_date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_15));
+  text_layer_set_background_color(s_date_layer, GColorClear);
+  text_layer_set_text_color(s_date_layer, GColorBlack);
+  text_layer_set_font(s_date_layer, s_date_font);
+  text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
+
+
+  // Add it as a child layer to the Window's root layer
+  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+  layer_add_child(window_layer, text_layer_get_layer(s_date_layer));
 }
 
-static void main_window_unload(Window *window) {
 
+static void main_window_unload(Window *window) {
+  // Destroy TextLayer
+  text_layer_destroy(s_time_layer);
+  text_layer_destroy(s_date_layer);
+
+  // Unload GFont
+  fonts_unload_custom_font(s_time_font);
+  fonts_unload_custom_font(s_date_font);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
+}
+
+static void update_time() {
+  // Get a tm structure
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+
+  // Write the current hours and minutes into a buffer
+  static char s_buffer[8];
+  strftime(s_buffer, sizeof(s_buffer), "%I:%M %p", tick_time);
+
+  // Display this time on the TextLayer
+  text_layer_set_text(s_time_layer, s_buffer);
+
+  // Write the date into a buffer
+  static char date_buffer[20];
+  strftime(date_buffer, sizeof(date_buffer), "%a %m/%d", tick_time);
+
+  // Display the date on the date layer
+  text_layer_set_text(s_date_layer, date_buffer);
 }
 
 static void init() {
-    // Create main Window element and assign to pointer
-    s_main_window = window_create();
+  // Create main Window element and assign to pointer
+  s_main_window = window_create();
 
-    // Set handlers to manage the elements inside the Window
-    window_set_window_handlers(s_main_window, (WindowHandlers) {
+  // Register with TickTimerService
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  
+  // Set handlers to manage the elements inside the Window
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
       .load = main_window_load,
       .unload = main_window_unload
-    });
+  });
+  
+  // Show the Window on the watch, with animated=true
+  window_stack_push(s_main_window, true);
 
-    // Show the Window on the watch, with animated=true
-    window_stack_push(s_main_window, true);
+  // Make sure the time is displayed from the start
+  update_time();
 }
 
 static void deinit() {
-    // Destroy Window
-    window_destroy(s_main_window);
+  // Unsubscribe from TickTimerService
+  tick_timer_service_unsubscribe();
+
+  // Destroy Window
+  window_destroy(s_main_window);
 }
 
 int main(void) {
-    init();
-    app_event_loop();
-    deinit();
+  init();
+  app_event_loop();
+  deinit();
+  return 0;
 }
